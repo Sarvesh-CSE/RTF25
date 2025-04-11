b482de2f664eaab1f5467c6af299d6f671cf73ce
sarvesh <pandeysarvesh100@gmail.com>
2025-04-10 17:49:36 -0700

    IG construction

--- ID Computation/ig_constructuon.py
+++ ID Computation/ig_constructuon.py
@@ -1,4 +1,6 @@
-# Importing necessary custom modules
+# # Importing necessary custom modules
+# import sys
+# sys.path.append('../DataGeneration')
 from dc_lookup import generate_lookup_table
 from proess_data import fetch_database_state, filter_data, get_target_cell_location, delset, target_eid
 
@@ -41,32 +43,44 @@ print("The phi: ", L)
 
 
 
-def construct_inference_graph(Phi, target_cell):
+def construct_inference_graph(Phi, delset):
     Gc = {}  # Adjacency list for the inference graph
     
     # Step 3: Get indexed constraints
     L = index_constraints(Phi)
     
-    # Step 4-5: Add deletable cells as nodes in Gc
+    # Step 4-5: Add deletable cells as nodes in Gc (and 6-7 combined)
     for ci in delset:
         Gc[ci] = set()
     print("Initial Inference Graph Gc:", Gc)
+
+    for ci in delset: # step 6
+        # Step 8: For each cell in delset, find the constraints it belongs to
+        if ci in L:
+            for phi in L[ci]:
+                # Step 9: For each constraint, find the cells involved in it
+                for cell in Phi[phi]:
+                    if cell != ci and cell in delset:
+                        Gc[ci].add(cell)
     
 
-    target_column = target_cell_location['column']
-    if target_column in L:
-        cells_phi = []
-        for phi in L[target_column]:
-            # Identify cells in delset that are part of the current constraint attributes
-            for cell in Phi[phi]:
-                if cell in delset:
-                    if cell not in cells_phi:
-                        cells_phi.append(cell)
 
-        for ci in cells_phi:
-            if ci != target_column:
-                Gc[ci].add(target_column)
-                print("Adding edge:", ci, target_column)
+# This is a workig code for the inference graph construction with respect to the target cell location. 
+# This is more optimized way of doing graph construction.
+    # target_column = target_cell_location['column']
+    # if target_column in L:
+    #     cells_phi = []
+    #     for phi in L[target_column]:
+    #         # Identify cells in delset that are part of the current constraint attributes
+    #         for cell in Phi[phi]:
+    #             if cell in delset:
+    #                 if cell not in cells_phi:
+    #                     cells_phi.append(cell)
+
+    #     for ci in cells_phi:
+    #         if ci != target_column:
+    #             Gc[ci].add(target_column)
+    #             print("Adding edge:", ci, target_column)
 
 
     # print("Inference Graph Gc:", Gc)
@@ -76,7 +90,7 @@ def construct_inference_graph(Phi, target_cell):
 
 
 # Construct the inference graph
-Gc = construct_inference_graph(Phi=Phi, target_cell=target_cell_location)
+Gc = construct_inference_graph(Phi=Phi, delset=delset)
 
 # Print the inference graph
 print(dict(Gc))
